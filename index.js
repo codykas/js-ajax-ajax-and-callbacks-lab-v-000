ef2eb9c46c912e543632fffc8d7b18f78febf23c
Cody Kasselman <ckasselman@fordham.edu>
2017-10-31 00:57:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,40 @@ function searchRepositories(){
   }
 ).fail(displayError())
 }
+
+function getSearchResults(data){
+  console.log(data)
+  return data.items.map(item =>
+    `
+      <div>
+        <h2><a href="${item.html_url}">${item.name}</a></h2>
+        <p>${item.description}</p>
+        <img src="${item.owner.avatar_url}" alt="${item.owner.login}">
+        <a href="${item.owner.html_url}">${item.owner.login}</a>
+        <p><a href="#" data-repository="${item.name}" data-owner="${item.owner.login}" onclick="showCommits(this)">Show Commits</a></p>
+      </div>
+    `)
+}
+
+function showCommits(item){
+  const url = `https://api.github.com/repos/${item.dataset.owner}/${item.dataset.repository}/commits`
+  console.log(url)
+  $.get(url, function(response){
+    $('#details').html(displayCommits(response))
+  }
+).fail(displayError())
+}
+
+function displayCommits(response){
+  console.log(response)
+  return response.map(commit =>
+    `
+      <div>
+        <h2>${commit.sha}</h2>
+      </div>
+    `)
+}
+
+function displayError(){
+  document.getElementById('errors').innerHTML = "Error!"
+}
