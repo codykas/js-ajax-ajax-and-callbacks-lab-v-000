e92538ad3f90b5455dbcb9d8322798169415ee83
Cody Kasselman <ckasselman@fordham.edu>
2017-10-31 00:51:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 $(document).ready(function (){
 });
+
+function searchRepositories(){
+  const term = document.getElementById('searchTerms').value
+  const url = `https://api.github.com/search/repositories?q=${term}`
+  $.get(url, function(response){
+    $('#results').html(getSearchResults(response))
+  }
+).fail(displayError())
+}
